0457676093f728efa43e9c7fd68ca66d26e094cc
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-09-10 00:01:49 +0000

    Adiciona pastas e config inicial

---
+++ api/api_v1/router.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+from handlers import user
+
+router = APIRouter()
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from core.config import settings
+from beanie import init_beanie
+from pymongo import AsyncMongoClient
+from models.user_model import User
 
-app = FastAPI()
+
+app = FastAPI(title=settings.PROJECT_NAME,
+              openapi_url=f"{settings.API_V1_STR}/openapi.json")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event("startup")
+async def on_startup():
+    client = AsyncMongoClient(settings.MONGO_CONNECTION_STRING)
+    await init_beanie(database=client.rpg_game, document_models=[User])
